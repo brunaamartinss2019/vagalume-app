9d73a9d468b538b87be6f8b5ba8709834ed5142d
bruna <brunaa.martins@gmail.com>
2026-04-19 22:06:57 +0200

    fix: remove PrivateRoute component, protect routes via auth context

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -8,20 +8,20 @@ import Layout from "./components/layout";
 import ReservationPage from "./pages/reservation-page";
 import { MyBookingsPage } from "./pages/dashboard";
 
-
 function App() {
- 
+
   return (
     <Layout>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/buscar" element={<SearchPage />} />
-      <Route path="/propiedad/:id" element={<PropertyDetailPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/registro" element={<RegisterPage />} />
-      <Route path="/reservar/:id" element={<ReservationPage />} />
-      <Route path="/dashboard/mis-reservas" element={<MyBookingsPage />} />
-    </Routes> 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/buscar" element={<SearchPage />} />
+        <Route path="/propiedad/:id" element={<PropertyDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/registro" element={<RegisterPage />} />
+
+        <Route path="/reservar/:id" element={<ReservationPage />} />
+        <Route path="/dashboard/mis-reservas" element={<MyBookingsPage />} />
+      </Routes>
     </Layout>
   );
 }
